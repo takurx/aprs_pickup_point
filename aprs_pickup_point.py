610393aff72aa5e08763f9e4be89a10a36619b98
takuwz <taku3wzr@gmail.com>
2019-01-12 17:50:12 +0900

    座標を抜き出せた。しかし無限ループ
    無限ループはCtrl+Cでも抜け出せない。

--- aprs_pickup_point.py
+++ aprs_pickup_point.py
@@ -1,4 +1,31 @@
+#
 # ref. 1,  http://d.hatena.ne.jp/thetama/20091103/1257213919
+#
+# - command example
+# chino@kafu:~$ direwolf -r 48000 udp:7355 | python aprs_pickup_point/aprs_pickup_point.py 
+#
+# - input example
+# JJ1BVF-7 audio level = 22(7/7)   [NONE]   ||||||___
+# [0.2] JJ1BVF-7>SVPUQ3,WIDE1-1:`D^Ol!?[/`"3{}_$<0x0d>
+# MIC-E, Human, Yaesu FT1D, Off Duty
+# N 36 05.1300, E 140 06.5100, 0 MPH, course 135, alt 30 ft
+# (repeat)
+#
+# - output example
+# N 36 05.1300, E 140 06.5100, 0 MPH, course 135, alt 30 ft
+# (repeat)
+#
+
 import sys
-input = sys.stdin.readline()
-print(input)
+
+while True:
+    try:
+        inline = sys.stdin.readline()
+        north = inline.find("N ")
+        south = inline.find("S ")
+        if north == 26 or south == 26:
+            sys.stdout.write(inline)
+        
+    except:
+        pass
+    
